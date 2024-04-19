8d7f1f043abc1d36460e951bcdf5bf6a5f3ccc8e
DESKTOP-JE6F1GA\admin <lostark9847@gmail.com>
2024-04-19 22:28:45 +0900

    프로필 누르면 링크이동에 임포트 짜잘한거 다 삭제

--- alphaknow/src/main/java/com/spring/alphaknow/controller/groupController.java
+++ alphaknow/src/main/java/com/spring/alphaknow/controller/groupController.java
@@ -1,22 +1,8 @@
 package com.spring.alphaknow.controller;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import com.spring.alphaknow.dto.boardDTO.BoardDTO;
 
 @Controller
 public class groupController {
