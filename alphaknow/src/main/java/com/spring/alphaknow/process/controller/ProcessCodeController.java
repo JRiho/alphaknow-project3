1c02a47935bea6da8d652123f38500534481e43d
DESKTOP-BCSJ0QE\jug12 <jug1227@naver.com>
2024-04-22 11:50:04 +0900

    바쁘다바빠

--- alphaknow/src/main/java/com/spring/alphaknow/process/controller/ProcessCodeController.java
+++ alphaknow/src/main/java/com/spring/alphaknow/process/controller/ProcessCodeController.java
@@ -3,12 +3,17 @@ package com.spring.alphaknow.process.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.spring.alphaknow.dto.processcodeDTO.ProcessCodeDTO;
@@ -47,5 +52,18 @@ public class ProcessCodeController {
         processCodeService.updateProcessCode(processCode);
         redirectAttributes.addFlashAttribute("message", "프로세스 코드 정보가 업데이트되었습니다!");
         return "redirect:/processcode";
+    }
+    
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    @ResponseBody
+    public ResponseEntity<?> deleteProcessCodeAjax(@PathVariable("id") int sequenceNo) {
+        try {
+            processCodeService.deleteProcessCode(sequenceNo);
+            return ResponseEntity.ok().body("프로세스 코드가 성공적으로 삭제되었습니다!");
+        } catch (DataAccessException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("서버 오류: " + e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body("삭제 실패: " + e.getMessage());
+        }
     }
 }
