ea1ed9119e021df2d882801f152ab679b7d051de
DESKTOP-BMCOBBN\admin <lmy0018@naver.com>
2024-04-23 12:45:02 +0900

    그룹쪽 좀 만짐

--- alphaknow/src/main/webapp/WEB-INF/views/group/project_calendar.jsp
+++ alphaknow/src/main/webapp/WEB-INF/views/group/project_calendar.jsp
@@ -7,77 +7,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="/alphaknow/resources/css/group.css">
+    <link rel="stylesheet" href="/alphaknow/resources/css/calendar.css">
+    <script src="/alphaknow/resources/js/calendar.js"></script>
 </head>
-<style>
-    .menu ul li a.calendar {
-        background: rgb(26, 188, 156);
-        color: #fff;
-        border: 0;
-        cursor: pointer;
-    }
 
-    .date {
-        position: relative;
-        align-items: center;
-        text-align: center;
-        margin-top: 10px;
-    }
-
-    .date img {
-        position: absolute;
-    }
-
-    .arrow1 {
-        max-height: 100%; 
-        transform: scaleX(-1);
-        width: 30px;
-        height: 30px;
-        left: 40%;
-        top: 20%;
-    }
-
-    .arrow2 {
-        max-height: 100%; 
-        width: 30px;
-        height: 30px;
-        right: 40%;
-        top: 20%;
-    }
-
-    .cal {
-        margin-top: 20px;
-    }
-
-    table {
-        width: 100%;
-        height: 600px;
-        border-collapse: collapse;
-        font-size: 14px;
-    }
-
-    tr, td {
-        padding: 8px;
-        vertical-align: top;
-        border-bottom: 2px solid #777;
-        border-top: 2px solid #777;
-    }
-
-    .save_but {
-        text-decoration: none;
-        margin-top: 20px;
-        font-size: 20px;
-        font-weight: bold;
-        width: 120px;
-        height: 40px;
-        border: 0; 
-        text-align: center; 
-        line-height: 40px;
-        background-color: rgb(190, 190, 190);
-        color: #fff;
-        border-radius: 10px;
-        cursor: pointer;
-    }
-</style>
 <body>
     <div class="side">
         <div class="side_profile">
@@ -106,43 +39,27 @@
             </ul>
         </div>
 
-        <div class="date">
+        <div class="calDate">
             <img src="/alphaknow/resources/img/arrow_icon.png"
             class="arrow1">
-            <font size="6"><b>2024.02</b></font>
+            <div class="year_month"></div>
             <img src="/alphaknow/resources/img/arrow_icon.png"
             class="arrow2">
         </div>
 
         <div class="cal">
-            <table>
-                <tr height="30">
-                    <td><font color="red">일</font></td> <td>월</td> <td>화</td> <td>수</td>
-                    <td>목</td> <td>금</td> <td><font color="blue">토</font></td>
-                </tr>
-                <tr>
-                    <td><font color="#999">28</font></td> <td><font color="#999">29</font></td> 
-                    <td><font color="#999">30</font></td> <td><font color="#999">31</font></td>
-                    <td>1</td> <td>2</td> <td><font color="blue">3</font></td>
-                </tr>
-                <tr>
-                    <td><font color="red">4</font></td> <td>5</td> <td>6</td> <td>7</td>
-                    <td>8</td> <td>9</td> <td><font color="blue">10</font></td>
-                </tr>
-                <tr>
-                    <td><font color="red">11</font></td> <td>12</td> <td>13</td> <td>14</td>
-                    <td>15</td> <td>16</td> <td><font color="blue">17</font></td>
-                </tr>
-                <tr>
-                    <td><font color="red">18</font></td> <td>19</td> <td>20</td> <td>21</td>
-                    <td>22</td> <td>23</td> <td><font color="blue">24</font></td>
-                </tr>
-                <tr>
-                    <td><font color="red">25</font></td> <td>26</td> <td>27</td> <td>28</td>
-                    <td>29</td> <td><font color="#999">1</font></td> 
-                    <td><font color="#999">2</font></td>
-                </tr>
-            </table>
+            <div class="main">
+				<div class="days">
+					<div class="day">일</div>
+					<div class="day">월</div>
+					<div class="day">화</div>
+					<div class="day">수</div>
+					<div class="day">목</div>
+					<div class="day">금</div>
+					<div class="day">토</div>
+				</div>
+				<div class="dates"></div>
+			</div>
         </div>
 
         <div style="text-align: right;">
--- alphaknow/src/main/webapp/resources/js/calendar.js
+++ alphaknow/src/main/webapp/resources/js/calendar.js
@@ -1,71 +1,78 @@
-window.onload = function() {
-    $(document).ready(function () {
-        // 날짜 선택 상자에 옵션을 채우는 함수
-        function populateSelectBoxes() {
-            for (var i = new Date().getFullYear(); i >= 1900; i--) {
-                $('#start-year, #end-year').append($('<option>', { value: i, text: i }));
-            }
-            for (var i = 1; i <= 12; i++) {
-                $('#start-month, #end-month').append($('<option>', { value: i, text: i }));
-            }
-            for (var i = 1; i <= 31; i++) {
-                $('#start-day, #end-day').append($('<option>', { value: i, text: i }));
+window.addEventListener('load', function () {
+
+    const date = new Date();
+
+    const renderCalendar = () => {
+        const viewYear = date.getFullYear();
+        const viewMonth = date.getMonth();
+
+        document.querySelector('.year_month').textContent = `${viewYear}.${viewMonth + 1}`;
+
+        const prevLast = new Date(viewYear, viewMonth, 0);
+        const thisLast = new Date(viewYear, viewMonth + 1, 0);
+
+        const PLDate = prevLast.getDate();
+        const PLDay = prevLast.getDay();
+
+        const TLDate = thisLast.getDate();
+        const TLDay = thisLast.getDay();
+
+        const prevDates = [];
+        const thisDates = [...Array(TLDate + 1).keys()].slice(1);
+        const nextDates = [];
+
+        if (PLDay !== 6) {
+            for (let i = 0; i < PLDay + 1; i++) {
+                prevDates.unshift(PLDate - i);
             }
         }
-
-        populateSelectBoxes();
-
-        // DatePicker 초기화 및 설정
-        function initDatepickers() {
-            $('#hidden-start-datepicker').datepicker({
-                dateFormat: "yy-mm-dd",
-                onSelect: function (dateText) {
-                    updateSelectBoxes(dateText, 'start');
-                }
-            });
-
-            $('#hidden-end-datepicker').datepicker({
-                dateFormat: "yy-mm-dd",
-                onSelect: function (dateText) {
-                    updateSelectBoxes(dateText, 'end');
-                }
-            });
+        for (let i = 1; i < 7 - TLDay; i++) {
+            nextDates.push(i)
         }
 
-       
-        
+        const dates = prevDates.concat(thisDates, nextDates);
+        const firstDateIndex = dates.indexOf(1);
+        const lastDateIndex = dates.lastIndexOf(TLDate);
 
-        // 셀렉트 박스 업데이트 함수
-        function updateSelectBoxes(dateText, prefix) {
-            var dateArray = dateText.split('-');
-            $('#' + prefix + '-year').val(dateArray[0]);
-            $('#' + prefix + '-month').val(parseInt(dateArray[1], 10)); // 월은 0 제거
-            $('#' + prefix + '-day').val(parseInt(dateArray[2], 10)); // 일은 0 제거
-        }
-
-        // 달력 아이콘 클릭 이벤트 처리
-        $('#start-calendar').click(function () {
-            $('#hidden-start-datepicker').datepicker('show');
+        dates.forEach((date, i) => {
+            const condition = i >= firstDateIndex && i < lastDateIndex + 1 ? 'this' : 'other';
+            dates[i] = `<div class="date"><span class = ${condition} > ${date}</span></div>`;
         });
 
-        $('#end-calendar').click(function () {
-            $('#hidden-end-datepicker').datepicker('show');
-        });
+        this.document.querySelector('.dates').innerHTML = dates.join('');
 
-        // '신청' 버튼 클릭 이벤트 처리
-        // $('#submit-button').click(function () {
-        //     showVacationModal();
-        // });
+        const today = new Date();
 
-        // // 모달 텍스트 설정 및 표시
-        // function showVacationModal() {
-        //     var startDate = $('#start-year').val() + '-' + $('#start-month').val() + '-' + $('#start-day').val();
-        //     var endDate = $('#end-year').val() + '-' + $('#end-month').val() + '-' + $('#end-day').val();
-        //     var message = startDate + ' ~ ' + endDate + '까지 휴가를 신청하시겠습니까?';
-        //     alert(message);
-        // }
+        if (viewMonth === today.getMonth() && viewYear === today.getFullYear()) {
+            for (let date of document.querySelectorAll('.this')) {
+                if (+date.innerText === today.getDate()) {
+                    date.classList.add('today');
+                }
+            }
+        }
+    }
+    renderCalendar();
+    document.querySelector('.arrow1').addEventListener('click', function () {
+        console.log('Previous month clicked'); // 디버깅을 위한 콘솔 로그
+        date.setMonth(date.getMonth() - 1);
 
-        initDatepickers();
+        renderCalendar();
     });
 
-}
+    document.querySelector('.arrow2').addEventListener('click', function () {
+        console.log('Next month clicked'); // 디버깅을 위한 콘솔 로그
+        date.setMonth(date.getMonth() + 1);
+
+        renderCalendar();
+    });
+
+    document.querySelectorAll('.date').forEach(date => {
+        date.addEventListener('click', function (e) {
+            const selectedDate = e.target.innerText;
+            // 선택된 날짜를 입력 폼에 설정
+            document.getElementById('selected-date').value = `${viewYear}-${viewMonth + 1}-${selectedDate}`;
+            // 입력 폼 표시
+            document.querySelector('.input-form').style.display = 'block';
+        });
+    });
+});
